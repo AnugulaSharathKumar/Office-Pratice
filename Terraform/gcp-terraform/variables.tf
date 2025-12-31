1e359975424974348cdd09688fb804fe3aa4a333
AnugulaSharathKumar <91570664+AnugulaSharathKumar@users.noreply.github.com>
2025-12-31 09:43:33 +0530

    Add GCP project variables in variables.tf
    Define variables for GCP project configuration.

---
+++ Terraform/gcp-terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "Your GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "VM machine type"
+  type        = string
+  default     = "e2-micro"
+}
